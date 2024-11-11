03ea42acbbea1a69cf6aa23dda56fdfb63bec61b
ConnerBowen <93921961+ConnerBowen@users.noreply.github.com>
2024-11-11 16:32:40 +0000

    css update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advanced Media Library - Your Gateway to Knowledge</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body class="bg-gray-50">
     <!-- Accessibility Skip Link -->
